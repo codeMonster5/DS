3c0481db10e8619707dc74dc41055bfef2620289
Mani <manibhushansingh20@gmail.com>
2020-09-14 15:36:16 +0530

    FlattenBinaryTree

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace DataStructure
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FlattenBinaryTree.BinaryTree tree = new FlattenBinaryTree.BinaryTree(4);
+            tree.left = new FlattenBinaryTree.BinaryTree(6);
+            tree.left.left = new FlattenBinaryTree.BinaryTree(7);
+            tree.left.right = new FlattenBinaryTree.BinaryTree(8);
+            tree.right = new FlattenBinaryTree.BinaryTree(9);
+            tree.right.left = new FlattenBinaryTree.BinaryTree(14);
+            tree.right.right = new FlattenBinaryTree.BinaryTree(16);
+
+            var value = FlattenBinaryTree.FlattenBinaryTrees(tree);
+
+            while(value.right != null)
+            {
+                Console.WriteLine(value.right.value);
+
+                value.right = value.right.right;
+            }
+
+            //Console.Write(value);
+
+            Console.ReadLine();
         }
     }
 }
